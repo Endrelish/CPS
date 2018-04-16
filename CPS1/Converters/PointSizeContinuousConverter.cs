2db20966887558c0cd8ec1ef9ac343650d5eea41
TagruP <purgat.pawel@gmail.com>
2018-04-16 11:04:31 +0200

    Chart displaying changes.

---
+++ CPS1/Converters/PointSizeContinuousConverter.cs
@@ -0,0 +1,39 @@
+﻿namespace CPS1.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows.Data;
+
+    public class PointSizeContinuousConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool cont)
+            {
+                if (cont == false)
+                {
+                    return 5.0d;
+                }
+
+                return 0.0d;
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is double size)
+            {
+                if (Math.Abs(size) < double.Epsilon)
+                {
+                    return true;
+                }
+
+                return false;
+            }
+
+            return null;
+        }
+    }
+}
