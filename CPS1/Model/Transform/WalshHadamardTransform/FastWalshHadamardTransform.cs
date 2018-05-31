e46e8dfc9f38d1073b172ec74840c2ac25286717
ciach-203860 <203860@edu.p.lodz.pl>
2018-05-31 19:30:30 +0200

    Add fast Walsh-Hadamard transform

--- CPS1/Model/Transform/WalshHadamardTransform/FastWalshHadamardTransform.cs
+++ CPS1/Model/Transform/WalshHadamardTransform/FastWalshHadamardTransform.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using CPS1.Model.SignalData;
 
 namespace CPS1.Model.Transform.WalshHadamardTransform
@@ -7,7 +9,38 @@ namespace CPS1.Model.Transform.WalshHadamardTransform
     {
         public override IEnumerable<Point> Transform(IEnumerable<Point> signal)
         {
-            throw new System.NotImplementedException();
+            var count = signal.Count();
+            var exp = Convert.ToInt32(Math.Log(count, 2));
+
+            var whTransform = WalshHadamardMatrix(exp - 1);
+            var points = new List<Point>(count);
+
+            var vector = new Matrix<double>(1, count / 2);
+
+            for (var i = 0; i < count / 2; i++)
+            {
+                vector[0, i] = points[i].Y + points[i + count / 2].Y;
+            }
+
+            var half = whTransform * vector;
+            var j = 0;
+            foreach (var d in half.ToList())
+            {
+                points.Add(new Point(j++, d));
+            }
+
+            for (var i = 0; i < count / 2; i++)
+            {
+                vector[0, i] = points[i].Y + -points[i + count / 2].Y;
+            }
+
+            half = whTransform * vector;
+            foreach (var d in half.ToList())
+            {
+                points.Add(new Point(j++, d));
+            }
+
+            return points;
         }
     }
 }
